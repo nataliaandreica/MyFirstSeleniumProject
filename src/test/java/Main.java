7686cd494eaebcf6a87c6dbbc8be489ca2a0beda
Natalia <nataliaandreica@yahoo.com>
2019-11-18 22:29:00 +0200

    tema

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,11 +1,41 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-    }
+        driver.get("http://testfasttrackit.info/selenium-test/");
 
-    }
+        WebElement saleLink = driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-5.parent > a"));
+        saleLink.click();
+        WebElement firstProduct = driver.findElement(By.cssSelector("#product-collection-image-423"));
+        firstProduct.click();
+        WebElement addToWhisList = driver.findElement(By.cssSelector("#product_addtocart_form > div.product-shop > div.product-options-bottom > ul.add-to-links > li:nth-child(1) > a"));
+        addToWhisList.click();}
+
+    public static void loginTest() {
+
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://testfasttrackit.info/selenium-test/");
+
+        WebElement accountLink = driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label"));
+        accountLink.click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+
+
+        driver.findElement(By.cssSelector("#email")).sendKeys("nataliaandreica@yahoo.com");
+
+        driver.findElement(By.cssSelector("#pass")).sendKeys("Windows123");
+
+        driver.findElement(By.cssSelector("#send2")).click();
+
+        driver.close();
+}}
+
+
 
