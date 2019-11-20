6f92d6d2307a2bd941d9bd5640f6cb47baf0863f
Natalia <nataliaandreica@yahoo.com>
2019-11-20 20:33:35 +0200

    JUnit

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -4,16 +4,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
-    public static void main(String[] args) {
+  /*  public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://testfasttrackit.info/selenium-test/");
 
-        WebElement saleLink = driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-5.parent > a"));
+    WebElement saleLink = driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-5.parent > a"));
         saleLink.click();
-        WebElement firstProduct = driver.findElement(By.cssSelector("#product-collection-image-423"));
+    WebElement firstProduct = driver.findElement(By.cssSelector("#product-collection-image-423"));
         firstProduct.click();
-        WebElement addToWhisList = driver.findElement(By.cssSelector("#product_addtocart_form > div.product-shop > div.product-options-bottom > ul.add-to-links > li:nth-child(1) > a"));
+    WebElement addToWhisList = driver.findElement(By.cssSelector("#product_addtocart_form > div.product-shop > div.product-options-bottom > ul.add-to-links > li:nth-child(1) > a"));
         addToWhisList.click();}
 
     public static void loginTest() {
@@ -34,8 +34,8 @@ public class Main {
 
         driver.findElement(By.cssSelector("#send2")).click();
 
-        driver.close();
-}}
+        driver.close(); */
+}
 
 
 
--- src/test/java/WishListTest.java
+++ src/test/java/WishListTest.java
@@ -1,9 +1,38 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WishListTest {
+    private WebDriver driver;
+
+    @Before
+    public void initDriver() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+
+    public void wishlistTest() {
+
+        driver.get("http://testfasttrackit.info/selenium-test/");
+
+        WebElement saleLink = driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-5.parent > a"));
+        saleLink.click();
+        WebElement firstProduct = driver.findElement(By.cssSelector("#product-collection-image-423"));
+        firstProduct.click();
+        WebElement addToWhisList = driver.findElement(By.cssSelector("#product_addtocart_form > div.product-shop > div.product-options-bottom > ul.add-to-links > li:nth-child(1) > a"));
+        addToWhisList.click();
+    }
+
+   @After
+    public void closeDriver() {
+        driver.quit();
 
 
+    }
 }
